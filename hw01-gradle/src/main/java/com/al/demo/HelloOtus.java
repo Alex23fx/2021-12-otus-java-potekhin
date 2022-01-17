91d5e7f40878ba7e7f21f923504ae6d77b6b6b6d
ALex23 <alex23fx@ya.ru>
2022-01-17 16:35:42 +0300

    реализация ДЗ № 1

--- hw01-gradle/src/main/java/com/al/demo/HelloOtus.java
+++ hw01-gradle/src/main/java/com/al/demo/HelloOtus.java
@@ -1,12 +1,11 @@
 package com.al.demo;
 
 import com.google.common.collect.Lists;
-import java.util.ArrayList;
-import java.util.List;
 
-public class Application {
+public class HelloOtus {
     public static void main(String... args) {
         System.out.println("Hello OTUS");
 
+        Lists.
     }
 }
