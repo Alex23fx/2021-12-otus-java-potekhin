119c374a39ee4b3002fc61b4f6221e00c8e8cac5
ALex23 <alex23fx@ya.ru>
2022-01-17 16:26:10 +0300

    Старт

---
+++ hw01-gradle/src/main/java/com/al/demo/HelloOtus.java
@@ -0,0 +1,12 @@
+package com.al.demo;
+
+import com.google.common.collect.Lists;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application {
+    public static void main(String... args) {
+        System.out.println("Hello OTUS");
+
+    }
+}
